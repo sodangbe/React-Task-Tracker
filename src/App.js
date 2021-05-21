1971bb7545b533f2d0c1d6483c88cd10b37430e3
romeo adjam <sodangbe@users.noreply.github.com>
2021-05-21 01:54:10 -0500

    Finished toggling

--- src/App.js
+++ src/App.js
@@ -31,10 +31,17 @@ const deleteTask = (id) =>{
   setTasks(tasks.filter((task) => task.id !== id))
 
 }
+
+//Toggle reminder
+const toggleReminder =(id) => {
+
+  setTasks(tasks.map((task) => task.id === id ? {...task, reminder:!task.reminder } : task))
+}
+
   return (
     <div className="container">
     <Header />
-    {tasks.length > 0 ? (<Tasks tasks={tasks}  onDelete={deleteTask} />) : ('No Tasks To Show')}
+    {tasks.length > 0 ? (<Tasks tasks={tasks}  onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show')}
     </div>
   );
 }
