dedd889ea9a278babac31ed6b127fbabd5e2ffc9
romeo adjam <sodangbe@users.noreply.github.com>
2021-05-21 01:17:10 -0500

    Finished deleting the task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,14 @@
+import { FaTimes } from 'react-icons/fa'
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete}) => {
     return (
         <div className='task'> 
-            <h3>{task.text}</h3>
-            <p>{task.day}</p>
+            <h3>
+                {task.text} < FaTimes style={{ color:'red', cursor:'pointer'}}  onClick={ () => onDelete(task.id)}/>
             
+            </h3>
+            <p>{task.day}</p>
+
         </div>
     )
 }
